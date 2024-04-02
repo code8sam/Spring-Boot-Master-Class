73211fe358bd1ec95af023ae0b5beac1de6f06ea
code8sam <work.social.satyam@gmail.com>
2024-04-03 00:00:36 +0530

    integrating retrofit and gradle in springboot

--- IntegratingRetroFitAndGradle/src/main/java/org/example/Main.java
+++ IntegratingRetroFitAndGradle/src/main/java/org/example/Main.java
@@ -1,19 +1,33 @@
 package org.example;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        OkHttpClient client = new OkHttpClient();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String url = "https://jsonplaceholder.typicode.com/todos/1/";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+                if (!response.isSuccessful()) {
+                    System.out.println("Something went wrong");
+                }
+                System.out.println(response.body().string());
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
