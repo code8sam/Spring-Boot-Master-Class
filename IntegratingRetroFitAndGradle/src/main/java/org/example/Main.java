6be86024697a5477165c547ba5d559e1aba57220
code8sam <work.social.satyam@gmail.com>
2024-04-03 18:57:37 +0530

    using RoboPOJOGenerator for creating java class for deserialization

--- IntegratingRetroFitAndGradle/src/main/java/org/example/Main.java
+++ IntegratingRetroFitAndGradle/src/main/java/org/example/Main.java
@@ -3,13 +3,9 @@ package org.example;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
