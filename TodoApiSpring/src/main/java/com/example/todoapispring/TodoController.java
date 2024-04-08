43e67f28b7e9085f0e30bb6d8f958b1174097514
code8sam <work.social.satyam@gmail.com>
2024-04-08 21:14:08 +0530

    adding method for POST req

--- TodoApiSpring/src/main/java/com/example/todoapispring/TodoController.java
+++ TodoApiSpring/src/main/java/com/example/todoapispring/TodoController.java
@@ -1,5 +1,8 @@
 package com.example.todoapispring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,14 +11,25 @@ import java.util.List;
 @RestController
 public class TodoController {
 
-    private static List<Todo> todos;
+    private static List<Todo> todoList;
 
     public TodoController(){
-        todos = new ArrayList<>();
-        todos.add(new Todo(1, false, "todo1", 1));
-        todos.add(new Todo(2, true, "todo2", 2));
+        todoList = new ArrayList<>();
+        todoList.add(new Todo(1, false, "todo1", 1));
+        todoList.add(new Todo(2, true, "todo2", 2));
     }
 
     // let's create our first API :
+
+    @GetMapping("/todos")
+    public List<Todo> getTodos(){
+        return todoList;
+    }
+
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo){
+        todoList.add(newTodo);
+        return newTodo;
+    }
 
 }
