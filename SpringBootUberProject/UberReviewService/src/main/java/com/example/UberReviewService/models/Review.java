d1a38a2244633514088b1fa43020194fd1fc1a07
code8sam <work.social.satyam@gmail.com>
2024-05-09 11:09:54 +0530

    added more columns to review

--- SpringBootUberProject/UberReviewService/src/main/java/com/example/UberReviewService/models/Review.java
+++ SpringBootUberProject/UberReviewService/src/main/java/com/example/UberReviewService/models/Review.java
@@ -1,12 +1,31 @@
 package com.example.UberReviewService.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import org.springframework.cglib.core.GeneratorStrategy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name="bookingrevuew")
 public class Review {
 
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     Long id;
+    @Column(nullable=false)
+    String content;
+    Double rating;
+    @Column(nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+    @Column(nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updatedAt;
+
 }
