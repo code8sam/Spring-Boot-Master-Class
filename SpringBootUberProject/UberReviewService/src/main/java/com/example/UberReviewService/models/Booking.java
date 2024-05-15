3b83b7db9ac1c824defd22ccc41daa458fddf689
code8sam <work.social.satyam@gmail.com>
2024-05-15 13:01:21 +0530

    added booking associated to review

---
+++ SpringBootUberProject/UberReviewService/src/main/java/com/example/UberReviewService/models/Booking.java
@@ -0,0 +1,27 @@
+package com.example.UberReviewService.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking extends BaseModel{
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH})
+    private Review review;
+    @Enumerated(value=EnumType.STRING)
+    private BookingStatus bookingStatus;
+    @Temporal(value= TemporalType.TIMESTAMP)
+    private Date startTime;
+    @Temporal(value= TemporalType.TIMESTAMP)
+    private Date endTime;
+    private Long totalDistance;
+
+
+}
--- SpringBootUberProject/UberReviewService/src/main/java/com/example/UberReviewService/services/ReviewService.java
+++ SpringBootUberProject/UberReviewService/src/main/java/com/example/UberReviewService/services/ReviewService.java
@@ -1,30 +1,43 @@
 package com.example.UberReviewService.services;
 
+import com.example.UberReviewService.models.Booking;
 import com.example.UberReviewService.models.Review;
+import com.example.UberReviewService.repositories.BookingRepository;
 import com.example.UberReviewService.repositories.ReviewRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Book;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewService implements CommandLineRunner {
     private ReviewRepository reviewRepository;
+    private final BookingRepository bookingRepository;
 
-    public ReviewService(ReviewRepository reviewRepository){
+    public ReviewService(ReviewRepository reviewRepository, BookingRepository bookingRepository){
         this.reviewRepository=reviewRepository;
+        this.bookingRepository=bookingRepository;
     }
     @Override
     public void run(String... args) throws Exception {
         System.out.println("********");
-        Review r = Review.builder().content("Amazing ride quality").rating(5.0).build();
-        reviewRepository.save(r);
-        System.out.println(r.getId());
-        List<Review> reviews = reviewRepository.findAll();
-        for(Review review : reviews){
-            System.out.println(r.getContent());
+//        Review r = Review.builder().content("Amazing ride quality").rating(5.0).build();
+//        Booking b = Booking.builder().review(r).endTime(new Date()).build();
+//        bookingRepository.save(b);
+////        reviewRepository.save(r);
+//
+//        System.out.println(r.getId());
+//        List<Review> reviews = reviewRepository.findAll();
+//        for(Review review : reviews){
+//            System.out.println(r.getContent());
+//        }
+//        reviewRepository.deleteById(2L);
+        Optional<Booking> b = bookingRepository.findById(702L);
+        if(b.isPresent()){
+            bookingRepository.delete(b.get());
         }
-        reviewRepository.deleteById(2L);
     }
 }
