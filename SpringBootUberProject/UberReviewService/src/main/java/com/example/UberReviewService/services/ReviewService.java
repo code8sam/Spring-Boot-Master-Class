fd54518909c71642dafe3435bf82a18c3e1d7c42
code8sam <work.social.satyam@gmail.com>
2024-05-28 02:23:15 +0530

    rawl query and hql

--- SpringBootUberProject/UberReviewService/src/main/java/com/example/UberReviewService/services/ReviewService.java
+++ SpringBootUberProject/UberReviewService/src/main/java/com/example/UberReviewService/services/ReviewService.java
@@ -1,8 +1,10 @@
 package com.example.UberReviewService.services;
 
 import com.example.UberReviewService.models.Booking;
+import com.example.UberReviewService.models.Driver;
 import com.example.UberReviewService.models.Review;
 import com.example.UberReviewService.repositories.BookingRepository;
+import com.example.UberReviewService.repositories.DriverRepository;
 import com.example.UberReviewService.repositories.ReviewRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
@@ -16,10 +18,12 @@ import java.util.Optional;
 public class ReviewService implements CommandLineRunner {
     private ReviewRepository reviewRepository;
     private final BookingRepository bookingRepository;
+    private DriverRepository driverRepository;
 
-    public ReviewService(ReviewRepository reviewRepository, BookingRepository bookingRepository){
+    public ReviewService(ReviewRepository reviewRepository, BookingRepository bookingRepository, DriverRepository driverRepository){
         this.reviewRepository=reviewRepository;
         this.bookingRepository=bookingRepository;
+        this.driverRepository=driverRepository;
     }
     @Override
     public void run(String... args) throws Exception {
@@ -35,9 +39,19 @@ public class ReviewService implements CommandLineRunner {
 //            System.out.println(r.getContent());
 //        }
 //        reviewRepository.deleteById(2L);
-        Optional<Booking> b = bookingRepository.findById(702L);
-        if(b.isPresent()){
-            bookingRepository.delete(b.get());
-        }
+//        Optional<Booking> b = bookingRepository.findById(702L);
+//        if(b.isPresent()){
+//            bookingRepository.delete(b.get());
+//        }
+//        Optional<Driver> driver = driverRepository.findByIdAndLicenseNumber(0L, "DL028765");
+//        if(driver.isPresent()){
+//            System.out.println(driver.get().getName());
+//            List<Booking> bookings = bookingRepository.findAllByDriverId(1L);
+//            for(Booking booking : bookings){
+//                System.out.println(booking.getBookingStatus());
+//            }
+//        }
+        Optional<Driver> d = driverRepository.hqlFindByIdAndLicense(0L, "DL028765");
+        System.out.println(d.get().getName());
     }
 }
